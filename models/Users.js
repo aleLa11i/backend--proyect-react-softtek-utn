af4219ab0bd18e540fc08bc4e481208831944aea
Alesandro Lalli <alesandro.lalli@gmail.com>
2022-05-10 13:13:55 -0300

    10/5-cambios

---
+++ models/Users.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require("mongoose");
+
+const Users = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: false
+  }
+});
+
+module.exports = model("User", Users);
