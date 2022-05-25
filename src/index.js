7353ecacb70ed4e42ce77f103873c2ae99b5d50f
Alesandro Lalli <alesandro.lalli@gmail.com>
2022-05-25 02:44:46 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ const morgan = require('morgan');
 const serverless = require('serverless-http');
 const { DBconnect } = require("./db/config");
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 app.use(cors());
 
 app.use(morgan('tiny'));
