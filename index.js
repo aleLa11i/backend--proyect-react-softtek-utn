cbf795ba93ff571dabbd6d613ae7807f66b17c44
Alesandro Lalli <alesandro.lalli@gmail.com>
2022-05-25 01:58:59 -0300

    netlify

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const cors = require("cors");
 const Auth = require("./routes/auth");
 const Posts = require("./routes/posts");
 const app = express();
+const router = express.Router();
 const morgan = require('morgan');
 const serverless = require('serverless-http');
 const { DBconnect } = require("./db/config");
 
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
-app.use(express.static("public"));
-app.use(express.json());
 
 app.use(morgan('tiny'));
 app.use("/api/auth", Auth);
 app.use("/api/posts", Posts);
+
+app.use('/.netlify/functions/api', router);
 
 DBconnect();
 
